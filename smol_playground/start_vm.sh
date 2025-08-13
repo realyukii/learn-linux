d6507fbf5b48fb7ee42e2cd20164ebf4a1ab0fea
realyukii <reyuki@gnuweeb.org>
2025-08-13 12:54:19 +0700

    qemu: Use virtual disk image for persistent storage
    The previous QEMU setup consisted only of a boot image, a Linux kernel executable,
    and an init.cpio file (for initramfs, also known as early user space [1]).
    
    This worked well for my typical “fire-and-forget” experiments and kernel
    playground sessions. However, it would be useful if the ash shell history could
    persist across runs. To achieve this, I modified `start_vm.sh` to let the user
    choose between a temporary play-around mode and a persistent setup.
    
    Unlike in early user space, I now need to specify the path to the init script
    by passing `init` as a kernel parameter [2].
    
    This change also removes all SSH-related lines from the configuration to
    emphasize that the purpose of this repository is to provide a minimal,
    usable Linux system. It’s up to the user to extend it beyond the provided
    basic setup.
    
    [1]:
    - https://en.wikipedia.org/wiki/Booting_process_of_Linux#cite_ref-18
    [2]:
    - https://raw.githubusercontent.com/torvalds/linux/master/Documentation/admin-guide/kernel-parameters.txt
    
    Signed-off-by: realyukii <reyuki@gnuweeb.org>

--- smol_playground/start_vm.sh
+++ smol_playground/start_vm.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 kernel_file="vmlinuz-linux"
+rootdisk_file="linux.img"
 initrd_file="init.cpio"
+opt="$1"
 
 if [ -z ${shared_dir+x} ]; then
 	shared_dir="./initramfs/portable"
@@ -13,28 +15,53 @@ if [ ! -f "./$kernel_file" ]; then
 	exit
 fi
 
-if [ ! -d "./initramfs/bin/" ]; then
+if [ ! -x "./initramfs/bin/busybox" ]; then
 	echo "[-] Please install busybox first"
 	exit
 fi
 
 dirs=(dev portable proc root sys)
 for dir in "${dirs[@]}"; do
-    mkdir -pv "./initramfs/$dir"
+	mkdir -pv "./initramfs/$dir"
 done
 
 kernel_ver=`file -bL ./$kernel_file | sed 's/.*version //;s/ .*//'`
 mkdir -pv "./initramfs/lib/modules/$kernel_ver"
-mkdir -pv ./initramfs/usr/{share/empty.sshd,lib/ssh}
 
-if [ ! -f "./$initrd_file" ]; then
-	echo "[+] generating $initrd_file"
-	(cd ./initramfs && find . | cpio -o -H newc -F "../$initrd_file")
-fi
+if [ "$opt" = "persistent" ]; then
+	echo "[+] Running in persistent mode"
 
-qemu-system-x86_64 \
+	if [ ! -f "./$rootdisk_file" ]; then
+		if [ $(id -u) -ne 0 ]; then
+			echo "[-] Please run as root to generate and bootstrap the VDI file"
+			exit
+		fi
+
+		dd if=/dev/zero of="./$rootdisk_file" bs=200M count=1 status=progress
+		mkfs.ext4 "./$rootdisk_file"
+		tmpdir=$(mktemp -d)
+		mount "./$rootdisk_file" "$tmpdir"
+		cp -r ./initramfs/* "$tmpdir"
+		umount "$tmpdir"
+		rm -rf "$tmpdir"
+	fi
+
+	qemu-system-x86_64 \
+	-drive file="./$rootdisk_file",format=raw,if=virtio \
+	-kernel "./$kernel_file" -append "root=/dev/vda init=/init console=ttyS0 rw" \
+	-m 2G -nographic -enable-kvm \
+	-virtfs local,path="$shared_dir",mount_tag=host0,security_model=passthrough,id=host0
+else
+	echo "[+] Running in non-persistent mode"
+
+	if [ ! -f "./$initrd_file" ]; then
+		echo "[+] generating $initrd_file"
+		(cd ./initramfs && find . | cpio -o -H newc -F "../$initrd_file")
+	fi
+
+	qemu-system-x86_64 \
 	-initrd "./$initrd_file" \
 	-kernel "./$kernel_file" -append "root=/ console=ttyS0" \
 	-m 2G -nographic -enable-kvm \
 	-virtfs local,path="$shared_dir",mount_tag=host0,security_model=passthrough,id=host0
-
+fi
